b0fd276a136133ba9796c35b0e17c23b2770cfb0
omarabdelfatah88 <omar.abdelfatah.h@gmail.com>
2021-11-17 21:53:53 +0200

    login signup screens

---
+++ lib/components/welcome_screen_image.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreenImage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Image(
+      image: AssetImage('images/welcomeScreenGif.gif'),
+      fit: BoxFit.cover,
+      height: double.infinity,
+      width: double.infinity,
+      alignment: Alignment.center,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:tumblrx/screens/signup_agecheck.dart';
 import 'package:tumblrx/screens/welcome_screen.dart';
 import 'package:tumblrx/services/authentication.dart';
 import 'package:tumblrx/services/content.dart';
@@ -7,6 +8,9 @@ import 'package:tumblrx/services/messaging.dart';
 import 'package:tumblrx/services/notifications.dart';
 import 'package:tumblrx/services/settings.dart';
 import 'package:tumblrx/services/theme.dart';
+import 'package:tumblrx/screens/welcome_screen_login.dart';
+import 'package:tumblrx/screens/welcome_screen_signup.dart';
+import 'package:tumblrx/screens/signup_pick_tags.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,29 +21,34 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        Provider<Authentication>(
+        ChangeNotifierProvider<Authentication>(
           create: (context) => Authentication(),
         ),
-        Provider<Content>(
+        ChangeNotifierProvider<Content>(
           create: (context) => Content(),
         ),
-        Provider<Messaging>(
+        ChangeNotifierProvider<Messaging>(
           create: (context) => Messaging(),
         ),
-        Provider<Notifications>(
+        ChangeNotifierProvider<Notifications>(
           create: (context) => Notifications(),
         ),
-        Provider<Settings>(
+        ChangeNotifierProvider<Settings>(
           create: (context) => Settings(),
         ),
-        Provider<Themes>(
+        ChangeNotifierProvider<Themes>(
           create: (context) => Themes(),
         ),
       ],
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         initialRoute: WelcomeScreen.id,
         routes: {
           WelcomeScreen.id: (context) => WelcomeScreen(),
+          LoginScreen.id: (context) => LoginScreen(),
+          SignupScreen.id: (context) => SignupScreen(),
+          SignUpAgeCheck.id: (context) => SignUpAgeCheck(),
+          SignUpPickTags.id: (context) => SignUpPickTags(),
         },
       ),
     );
